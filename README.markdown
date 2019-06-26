38f73fc423e3895890703b8a60c4a5a949e5afaf
Jason Owen <jason@jasonaowen.net>
2019-06-26 18:09:39 -0400

    Add initial C program, CMake config, and gitigore

--- README.markdown
+++ README.markdown
@@ -2,4 +2,16 @@
 
 A simple service to listen for geoclue events and log them to PostgreSQL.
 
+## Building
+
+This project uses the [CMake](https://cmake.org/) build system.
+
+```ShellSession
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+$ ./geoclue-psql
+```
+
 [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
---
+++ geoclue-psql.c
@@ -0,0 +1,21 @@
+/*
+ *  geoclue-psql: listen for geoclue events and log them to PostgreSQL
+ *  Copyright (C) 2019 Jason Owen <jason@jasonaowen.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+int main(int argc, char* argv[]) {
+  return 0;
+}
